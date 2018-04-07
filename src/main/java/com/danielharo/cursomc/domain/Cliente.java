cd8e99f036dd31c9e99bf7adc9b51f6b87e5cc21
Daniel Haro <contato.danielharo@gmail.com>
2018-04-07 19:29:44 -0300

    Implementando Pedido, Pagamento, PagamentoComCartao, PagamentoComBoleto

--- src/main/java/com/danielharo/cursomc/domain/Cliente.java
+++ src/main/java/com/danielharo/cursomc/domain/Cliente.java
@@ -38,6 +38,8 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 	}
 
@@ -129,6 +131,14 @@ public class Cliente implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 	
 
